78e77682345cdef612f5c4c9064d4557f3f4daa6
Jeff Franklin <jpf@uw.edu>
2016-03-07 10:56:51 -0800

    Run travis tests on Django pre-1.8 and post-1.9.

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^oauth/', include('oauth_provider.urls')),
+    # url(r'^oauth/', include('oauth_provider.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('nagios_registration.urls')),
+    # url(r'^', include('nagios_registration.urls')),
 )
